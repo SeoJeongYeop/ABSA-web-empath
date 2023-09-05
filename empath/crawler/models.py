4a17b65c83244a8952325240e8dd7f9ae4308d21
SeoJeongYeop <sjyswe99@gmail.com>
2023-09-05 06:40:31 +0000

    feat: 유튜브 검색어, 영상, 댓글, 채널 모델 추가

--- empath/crawler/models.py
+++ empath/crawler/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class SearchResult(models.Model):
+    id = models.AutoField(primary_key=True)
+    keyword = models.CharField(max_length=100)
+    video_id = models.CharField(max_length=20)
+    crawled_at = models.DateTimeField(auto_now_add=True)
+
+
+class YoutubeVideo(models.Model):
+    id = models.AutoField(primary_key=True)
+    video_id = models.CharField(max_length=20, unique=True, db_index=True)
+    title = models.CharField(max_length=200)
+    published_at = models.DateTimeField()
+    description = models.TextField()
+    channel_id = models.CharField(max_length=32)
+    channel_title = models.CharField(max_length=100)
+    tags = models.TextField()
+    category_id = models.IntegerField()
+    comment_count = models.IntegerField()
+    crawled_at = models.DateTimeField(auto_now_add=True)
+
+
+class YoutubeComment(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment = models.TextField()
+    video = models.ForeignKey(YoutubeVideo, models.CASCADE)
+    crawled_at = models.DateTimeField(auto_now_add=True)
+
+
+class YoutubeChannel(models.Model):
+    id = models.AutoField(primary_key=True)
+    channel_id = models.CharField(max_length=32, unique=True, db_index=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    published_at = models.DateTimeField()
+    subscriber_count = models.IntegerField()
+    video_count = models.IntegerField()
+    crawled_at = models.DateTimeField(auto_now_add=True)
