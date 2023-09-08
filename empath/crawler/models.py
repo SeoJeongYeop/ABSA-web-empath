26531c7b2a3309c73eb36a56c3262ee3b61baebe
SeoJeongYeop <sjyswe99@gmail.com>
2023-09-08 13:35:45 +0000

    feat: Naver 기사 관련 Django 모델 추가 - Naver 검색결과 모델, Naver 기사 모델 추가 - Youtube 검색결과 모델 이름 변경

--- empath/crawler/models.py
+++ empath/crawler/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class SearchResult(models.Model):
+class YoutubeSearchResult(models.Model):
     id = models.AutoField(primary_key=True)
     keyword = models.CharField(max_length=100)
     video_id = models.CharField(max_length=20)
@@ -37,4 +37,25 @@ class YoutubeChannel(models.Model):
     published_at = models.DateTimeField()
     subscriber_count = models.IntegerField()
     video_count = models.IntegerField()
+    crawled_at = models.DateTimeField(auto_now_add=True)
+
+
+class NaverSearchResult(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    keyword = models.CharField(max_length=100)
+    press_name = models.CharField(max_length=20)
+    link = models.CharField(max_length=200)
+    summary = models.CharField(max_length=200)
+    crawled_at = models.DateTimeField(auto_now_add=True)
+
+
+class NaverNewsArticle(models.Model):
+    id = models.AutoField(primary_key=True)
+    oid = models.CharField(max_length=4)
+    aid = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_at = models.DateTimeField()
+    press_name = models.CharField(max_length=20)
     crawled_at = models.DateTimeField(auto_now_add=True)
