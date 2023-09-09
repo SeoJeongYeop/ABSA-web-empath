22be4b08731ce8320953f9719b64ce35567f6613
SeoJeongYeop <sjyswe99@gmail.com>
2023-09-09 09:26:53 +0000

    feat: 텍스트 전처리 함수 개선 - emoji를 제거하는 함수를 분리하여 추가 - 중복 줄바꿈을 제거하는 함수 추가

--- empath/crawler/scrapy/empath/preprocess.py
+++ empath/crawler/scrapy/empath/preprocess.py
@@ -5,26 +5,42 @@ def remove_escape(text):
     return re.sub(r'[\\][n|t|r]', ' ', text)
 
 
+def reduce_escape(text):
+    text = re.sub(r'(\\n|\\r){2,}', '\\n', text)
+    text = re.sub(r'(\n|\r){2,}', '\n', text)
+    return text
+
+
+def remove_emoji(text):
+    emojis = []
+    # emoji 특수기호 + ^^ :)와 같은 이모티콘
+    emoji = re.findall(
+        r'[\u2700-\u27BF]|[\u2011-\u26FF]|[\u2900-\u2b59]|[!-/:-@\[-`]{2,}', text, flags=re.UNICODE)
+    if len(emoji) > 0:
+        emojis.append(emoji)
+    # (powerful) 한글, 영어, 숫자, 페리오드, 콤마 남기고 제거, 자모 제거하지 않음
+    text = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9 ]', ' ', text)
+    text.replace(',', '')
+    text = re.sub(' +', ' ', text).strip()
+
+    return text, emojis
+
+
 def remove_jamo(text):
-    jamolist = []
-    emojilist = []
+    jamos = []
 
     # 중복 띄어쓰기 제거
     text = re.sub(' +', ' ', text).strip()
     jamo = re.findall(r'[ㄱ-ㅎㅏ-ㅣ ]{2,}', text)
     if len(jamo) > 0:
-        jamolist.append(jamo)
-    # emoji 특수기호 + ^^ :)와 같은 이모티콘
-    emoji = re.findall(
-        r'[\u2700-\u27BF]|[\u2011-\u26FF]|[\u2900-\u2b59]|[!-/:-@\[-`]{2,}', text, flags=re.UNICODE)
-    if len(emoji) > 0:
-        emojilist.append(emoji)
+        jamos.append(jamo)
+
     # 한글, 영어, 숫자, 페리오드, 콤마 남기고 제거, 자모도 제거
     text = re.sub(r'[^가-힣a-zA-Z0-9., ]', ' ', text)
     # 중복 띄어쓰기 제거
     text = re.sub(' +', ' ', text).strip()
 
-    return text, jamolist, emojilist
+    return text, jamos
 
 
 def cleaning_text(text):
@@ -44,8 +60,6 @@ def cleaning_text(text):
     text = re.sub(r'[.,]{4,}', '... ', text)
     text = re.sub(r'[.]{2}', '.', text)
 
-    # (powerful) 한글, 영어, 숫자, 페리오드, 콤마 남기고 제거, 자모 제거하지 않음
-    # text = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9 ]', ' ', text)
     text = re.sub(r'[ㅜㅠ]{2,}', 'ㅠㅠ', text)
     text = re.sub(r'[ㄱㅋㅌ⫬]{2,}', 'ㅋㅋ', text)
     # 위에 해당하지 않고 자모 3개 이상 제거
