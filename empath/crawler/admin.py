e2481ff41bfbf3183a18f031e5871cf1e7f986ff
SeoJeongYeop <sjyswe99@gmail.com>
2023-10-20 12:36:28 +0000

    feat: Django 데이터 관리를 위한 Admin 세팅 추가

--- empath/crawler/admin.py
+++ empath/crawler/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from crawler.models import NaverSearchResult, NaverNewsArticle, YoutubeSearchResult, YoutubeVideo, YoutubeVideoComment
+from import_export.admin import ImportExportMixin
 
-# Register your models here.
+
+class NaverSearchResultAdmin(ImportExportMixin, admin.ModelAdmin):
+    search_fields = ["id", "title", "keyword", "press_name"]
+    list_display = ("id", "title", "keyword",
+                    "press_name", "summary")
+
+
+class NaverNewsArticleAdmin(ImportExportMixin, admin.ModelAdmin):
+    search_fields = ["id", "title", "content", "press_name"]
+    list_display = ("id", "title", "press_name", "content", "published_at")
+
+
+class YoutubeSearchResultAdmin(ImportExportMixin, admin.ModelAdmin):
+    search_fields = ["id", "keyword", "video_id"]
+    list_display = ("id", "keyword", "video_id", "crawled_at")
+
+
+class YoutubeVideoAdmin(ImportExportMixin, admin.ModelAdmin):
+    search_fields = ["id", "video_id", "title", "channel_title"]
+    list_display = ("id", "video_id", "title",
+                    "published_at", "description", "channel_id", "channel_title", "tags", "category_id", "comment_count")
+
+
+class YoutubeVideoCommentAdmin(ImportExportMixin, admin.ModelAdmin):
+    search_fields = ["id", "content", "video_id"]
+    list_display = ("id", "content", "video_id", "crawled_at")
+
+
+admin.site.register(NaverSearchResult, NaverSearchResultAdmin)
+admin.site.register(NaverNewsArticle, NaverNewsArticleAdmin)
+admin.site.register(YoutubeSearchResult, YoutubeSearchResultAdmin)
+admin.site.register(YoutubeVideo, YoutubeVideoAdmin)
+admin.site.register(YoutubeVideoComment, YoutubeVideoCommentAdmin)
