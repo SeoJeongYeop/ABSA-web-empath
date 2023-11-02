2e9bcb4ca2df3171eec705d934d0206ebfb49687
SeoJeongYeop <sjyswe99@gmail.com>
2023-11-02 12:44:44 +0000

    feat: 문장 분석 컴포넌트 추가

--- empath/static/js/vis.js
+++ empath/static/js/vis.js
@@ -1,10 +1,11 @@
-let BarCharts;
+let wordBarCharts,
+  sentimentBarChart = [];
 $(function () {
   const link = location.href;
   d3.json(`${link}word-count/`).then((jsonData) => {
     const countData = getKeywordCount(jsonData);
 
-    BarCharts = countData.map((countObj) => {
+    wordBarCharts = countData.map((countObj) => {
       return Object.keys(countObj).map((key) => {
         const obj = Object.values(countObj[key])[0];
         const bc = new Barchart(`#word-count-${key} svg`);
@@ -31,7 +32,8 @@ $(function () {
         } else if (sentiments[keyword].status === 'run') {
           $(`#infer-${keyword}`).html('감성분석 중입니다.');
         } else {
-          renderTriplet(jsonData.triplets);
+          $(`#infer-${keyword} .spinner-border`).remove();
+          renderTriplet(jsonData.triplets[keyword], keyword);
         }
       });
     },
@@ -69,6 +71,76 @@ function updateBarChart(chart, obj) {
   else chart.update(obj, Object.keys(obj));
 }
 
-function renderTriplet(triplets) {
+function renderTriplet(triplets, keyword) {
   console.log('renderTriplet', triplets);
+  const sentimentCount = countSentiments(triplets, keyword);
+  console.log(sentimentCount);
+  const bc = new Barchart(`#sentiment-${keyword} svg`);
+  bc.initialize();
+  updateBarChart(bc, sentimentCount);
+  sentimentBarChart.push(bc);
+}
+
+function countSentiments(data, keyword) {
+  const keywordData = data;
+  const sentimentCounts = {
+    positive: 0,
+    negative: 0,
+    neutral: 0
+  };
+  let body = '';
+
+  keywordData.forEach((item, i) => {
+    const polarities = item.polarities;
+    const aspects = item.aspects;
+    const opinions = item.opinions;
+    const raw = item.raw_sentence;
+
+    for (let i = 0; i < polarities.length; i++) {
+      const pol = polarities[i];
+      const asp = aspects[i];
+      const opn = opinions[i];
+      let polClass;
+      if (pol === '<pos>') {
+        sentimentCounts.positive++;
+        polClass = 'primary';
+      } else if (pol === '<neg>') {
+        sentimentCounts.negative++;
+        polClass = 'danger';
+      } else if (pol === '<neu>') {
+        sentimentCounts.neutral++;
+        polClass = 'secondary';
+      } else {
+        continue;
+      }
+      body += `<div class="accordion-body">
+          <span class="badge bg-pastel-${polClass} text-${polClass}">
+          ${pol.slice(1, 4)}
+          </span>
+          <div>${asp} <i class="bi bi-arrow-right-short"></i>${opn}</div>
+        </div>`;
+    }
+    $(`#accordion-${keyword}`).append(
+      `<div class="accordion-item">
+      <h2 class="accordion-header">
+        <button
+          class="accordion-button gap-2 collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#col-${keyword}-${i}"
+          aria-expanded="false"
+          aria-controls="col-${keyword}-${i}"
+        >
+          ${raw}
+        </button>
+      </h2>
+      <div id="col-${keyword}-${i}" class="accordion-collapse collapse bg-gray-200" data-bs-parent="#accordion-${keyword}">
+        ${body}
+      </div>
+    </div>`
+    );
+    body = '';
+  });
+
+  return sentimentCounts;
 }
